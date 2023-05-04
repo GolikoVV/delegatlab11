3f6441cd02be4304b95d3d8d8a902f1e56588a54
Лера Голикова <funtik-98@mail.ru>
2023-05-04 09:58:16 +0700

    Добавьте файлы проекта.

---
+++ delegatlab11/MathExpressions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class MathExpressions
+{
+    public static double Add(double x) => x + 5.0;
+    public static double Subtract(double x) => x - 2.5;
+    public static double Multiply(double x) => x * 2.0;
+    public static double Divide(double x) => x / 3.0;
+    public static double Power(double x) => Math.Pow(x, 2.0);
+
+    public delegate double MathDelegate(double x);
+
+    public MathDelegate[] Methods { get; } = new MathDelegate[]
+    {
+        Add,
+        Subtract,
+        Multiply,
+        Divide,
+        Power
+    };
+}
+/*
+ В данном примере класс MathExpressions содержит методы для вычисления математических выражений 
+и массив делегатов Methods, в котором хранятся ссылки на эти методы. В классе определен делегат MathDelegate, 
+который принимает один параметр типа double и возвращает значение типа double.
+ */
---
+++ delegatlab11/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        MathExpressions expr = new MathExpressions();
+        double result = 10.0;
+
+        for (int i = 0; i < 3; i++)
+        {
+            foreach (var method in expr.Methods)
+            {
+                result = method(result);
+                Console.WriteLine($"Result after {method.Method.Name}: {result}");
+            }
+        }
+    }
+}
+
+/* В методе Main создается экземпляр класса MathExpressions и переменная result с начальным значением 10. 
+ * Затем происходит последовательный вызов методов из массива делегатов, используя значение result в качестве параметра ввода. 
+ * Результат каждого вычисления выводится в консоль. 
+ * В данном примере это происходит три раза, чтобы продемонстрировать повторное использование делегатов.
+ * 
+ */
